ac2ddd09b52de3e16eea97fa7f450452d182d868
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2023-09-07 01:47:50 +0530

    added responsiveness for mobile

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Social from './Social'
+
 import './css/content.css'
 
 function Content() {
   return (
     <div className='content-box'>
-        <Social/>
         <div className='main-content'>
             <div className='image'/>
             <div className='about-box'>
@@ -15,25 +14,25 @@ function Content() {
                     <div className='skill-holder'>
                         <button className='skill-button'>
                             <h4 className='button-text'>Web Developer</h4>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" className="bi bi-x" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-x close-but" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                             </svg>
                         </button>
                         <button className='skill-button'>
                             <h4 className='button-text'>Graphic Designer</h4>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" className="bi bi-x" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-x close-but" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                             </svg>
                         </button>
                         <button className='skill-button'>
                             <h4 className='button-text'>UI/UX Designer</h4>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" className="bi bi-x" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-x close-but" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                             </svg>
                         </button>
                         <button className='skill-button'>
                             <h4 className='button-text'>Flutter Developer</h4>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" className="bi bi-x" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-x close-but" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                             </svg>
                         </button>
@@ -44,19 +43,19 @@ function Content() {
                     <div className='skill-holder'>
                         <button className='skill-button skill-but-2'>
                             <h4 className='button-text'>Vocalist</h4>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" className="bi bi-x" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-x close-but" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                             </svg>
                         </button>
                         <button className='skill-button skill-but-2'>
                             <h4 className='button-text'>Instrumentalist</h4>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" className="bi bi-x" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-x close-but" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                             </svg>
                         </button>
                         <button className='skill-button skill-but-2'>
                             <h4 className='button-text'>Film Critic</h4>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" className="bi bi-x" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-x close-but" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                             </svg>
                         </button>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,52 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from 'react'
 import './css/header.css'
-import sdlogo from '/src/assets/icons/sd_logo.png'
+import sdlogo from '/src/assets/icons/main-logo.png'
+import Social from './Social'
+
 
 function Header() {
+
+  // eslint-disable-next-line no-unused-vars
+  const [caretStateOpen, setCaretState] = useState(false)
+
+  const toggleCaret = () => {
+    setCaretState(!caretStateOpen)
+  }
+
   return (
     <header className='header'>
-        <img src={sdlogo} className='sd-logo'/>
-        <nav className='nav-bar'>
-          <a className='nav-link' href=''>About</a>
-          <a className='nav-link' href=''>Projects</a>
-          <a className='nav-link' href=''>Hobbies</a>
-          <a className='nav-link' href=''>Experience</a>
-          <a className='nav-link' href=''>Contact</a>
-          <button className='cv-button'>Download CV</button>
-        </nav>
+      <div className='head-div'>
+          <img src={sdlogo} className='sd-logo'/>
+          <nav className='nav-bar'>
+            <a className='nav-link' id='about' href=''>About</a>
+            <a className='nav-link' id='projects' href=''>Projects</a>
+            <a className='nav-link' id='experience' href=''>Experience</a>
+            <a className='nav-link' id='hobbies' href=''>Hobbies</a>
+            <a className='nav-link' id='contact' href=''>Contact</a>
+            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-caret-down-fill caret" id='nav-caret' onClick={toggleCaret} viewBox="0 0 16 16">
+              <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+            </svg>
+            <a href='/pdf/Resume.pdf' download="Resume"><button className='cv-button'>Download CV</button></a>
+          </nav>
+        </div>
+        <div className='link-overflow' >
+          <a className='link-overflow-item nav-link' href='./'>Hobbies</a>
+          <a className='link-overflow-item nav-link' href='./'>Contact</a>
+        </div>
+        <Social/>
+        <style >{`
+          #nav-caret{
+            rotate: ${caretStateOpen ? '180deg' : '0deg'};
+            transition: 0.2s;
+          }
+
+          .link-overflow{
+            display: ${caretStateOpen ? 'flex' : 'none'};
+            transition: height 0.2s;
+          }
+        `}</style>
     </header>
   )
 }
