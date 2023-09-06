383225225308516c24c04e825858166b6b8cd3ae
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2023-09-06 13:00:07 +0530

    fixed image links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './css/header.css'
+import sdlogo from '/src/assets/icons/sd_logo.png'
 
 function Header() {
   return (
     <header className='header'>
-        <img src='src/assets/icons/sd_logo.png' className='sd-logo'/>
+        <img src={sdlogo} className='sd-logo'/>
         <nav className='nav-bar'>
           <a className='nav-link' href=''>About</a>
           <a className='nav-link' href=''>Projects</a>
