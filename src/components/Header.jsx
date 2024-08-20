d019724dfc287dc02930f0ec2e5e2b79a80f9e99
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2024-08-20 06:03:46 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,7 +48,7 @@ function Header({nav}) {
                 <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
               </svg>
             </div>
-            <a href='/pdf/Resume.pdf' download="Resume"><button className='cv-button'>Download CV</button></a>
+            <a href='/pdf/resume2.pdf' download="Resume"><button className='cv-button'>Download CV</button></a>
           </nav>
         </div>
         <div className='link-overflow' >
